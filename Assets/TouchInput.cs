65ebc01d0232f583e34e7775cc6b45d0d69c06e5
fujisawa <sfujisawa2016@gmail.com>
2018-11-26 11:24:22 +0900

    資料６、スプライト発生まで

--- Assets/TouchInput.cs
+++ Assets/TouchInput.cs
@@ -5,13 +5,21 @@ using UnityEngine.UI;
 
 public class TouchInput : MonoBehaviour {
 
-    // フィールド
+    // フィールド部
+
+    // テキストコンポーネント
     private Text textComponent;
+    // メインカメラ
+    private Camera cam;
+    // 画像のプレハブ
+    public GameObject prefab;
 
     // Use this for initialization
     void Start () {
         // テキストコンポーネントを取得
         textComponent = GetComponent<Text>();
+        // メインカメラを取得
+        cam = Camera.main;
     }
 	
 	// Update is called once per frame
@@ -19,15 +27,55 @@ public class TouchInput : MonoBehaviour {
         // 一か所以上のタッチ情報あり？
         if (Input.touchCount > 0)
         {
-            // タッチの取得
-            Touch touch = Input.GetTouch(0);
+            // 文字列
+            string text = "";
+            foreach (Touch touch in Input.touches)
+            {
+                // タッチ座標をスクリーン座標系からワールド座標系に変換
+                Vector2 worldpos = cam.ScreenToWorldPoint(touch.position);
+                // 文字列の後ろに文字を追加
+                text += "touch" + touch.fingerId + ":("
+                    + worldpos.x + ","
+                    + worldpos.y + ")" + "\n";
 
-            textComponent.text =
-                "touch:(" + touch.position.x + "," + touch.position.y + ")";
+                if (touch.phase == TouchPhase.Began)
+                {
+                    // 指定したワールド座標にプレハブから複製
+                    Instantiate(prefab, worldpos, Quaternion.identity);
+                }
+
+                // 文字列の後ろに文字を追加
+
+                //// フェーズによる分岐
+                //switch (touch.phase)
+                //{
+                //    case TouchPhase.Began://押した瞬間
+                //        text = "Began";
+                //        break;
+                //    case TouchPhase.Moved://動いたとき
+                //        text = "Moved";
+                //        break;
+                //    case TouchPhase.Stationary://動いてないとき
+                //        text = "Stationary";
+                //        break;
+                //    case TouchPhase.Ended://離した瞬間
+                //        text = "Ended";
+                //        break;
+                //    case TouchPhase.Canceled://キャンセルされた瞬間
+                //        text = "Canceled";
+                //        break;
+                //}
+            }
+            textComponent.text = text;
         }
         else
         {
             textComponent.text = "No touch";
         }
+
+        //string str1;
+        // 文字列に数字を足すと、
+        // 文字列に数字がつながる
+        //str1 = "mojiretsu" + 1;
 	}
 }
